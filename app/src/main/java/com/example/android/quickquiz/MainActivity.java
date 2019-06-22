4e92f27c1b02a23114550a0b2ca093b292350f5c
Nada <little.nado12@gmail.com>
2019-06-22 02:52:58 +0200

    connecting actions with XML code

--- app/src/main/java/com/example/android/quickquiz/MainActivity.java
+++ app/src/main/java/com/example/android/quickquiz/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.android.quickquiz;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-     int score;
+     String name;
+     int score=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,93 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calculateScore(View view){
+        EditText userName = (EditText) findViewById(R.id.editText);
+        String name = userName.getText().toString();
+        if(name.isEmpty()){
+            Toast.makeText(this, "please enter your name! ", Toast.LENGTH_LONG).show();
+        }
+
+
+
+        EditText age = findViewById(R.id.editText2);
+        String ageNumber = age.getText().toString();
+
+        if(!ageNumber.isEmpty()){
+            score = score+1;
+        }
+        else {
+            Toast.makeText(this, "please enter your Age " + name + "!", Toast.LENGTH_LONG).show();
+        }
+
+        CheckBox alaska = findViewById(R.id.checkBox);
+        CheckBox newYork = findViewById(R.id.checkBox2);
+        CheckBox losAngeles = findViewById(R.id.checkBox3);
+        if(alaska.isChecked()&& !losAngeles.isChecked() && !newYork.isChecked()){
+            score += 1;
+        }
+
+        RadioButton rb2 = findViewById(R.id.radioButton2);
+        RadioButton rb1 = findViewById(R.id.radioButton);
+        if(rb1.isChecked() && !rb2.isChecked()){
+            score +=1;
+        }else
+            Toast.makeText(this, "Answer question no.2" + name + "!", Toast.LENGTH_LONG).show();
+
+
+
+        RadioButton rb3 = findViewById(R.id.radioButton3);
+        RadioButton rb4 = findViewById(R.id.radioButton4);
+        if(rb4.isChecked() && !rb3.isChecked()){
+            score +=1;
+        }else
+            Toast.makeText(this, "Answer question no.3 " + name + "!", Toast.LENGTH_LONG).show();
+        
+
+        RadioButton rb5 = findViewById(R.id.radioButton5);
+        RadioButton rb6 = findViewById(R.id.radioButton6);
+        if(rb6.isChecked() && !rb5.isChecked()){
+            score +=1;
+        }else if(!rb6.isChecked() && !rb5.isChecked())
+            Toast.makeText(this, "Answer question no.4 " + name + "!", Toast.LENGTH_LONG).show();
+
+
+
+        EditText ed3 = findViewById(R.id.editText3);
+        String editText = ed3.getText().toString();
+
+        if(!editText.isEmpty() && editText == "stomach"){
+            score = score+1;
+        }
+        else if(editText.isEmpty()){
+            Toast.makeText(this, "Answer question no.5 " + name + "!", Toast.LENGTH_LONG).show();
+        }
+
+        CheckBox ducks = findViewById(R.id.checkBox4);
+        CheckBox lions = findViewById(R.id.checkBox5);
+        CheckBox elephants = findViewById(R.id.checkBox6);
+        if(ducks.isChecked()&& !lions.isChecked() && !elephants.isChecked()){
+            score += 1;
+        }else if(!ducks.isChecked() && !lions.isChecked() && !elephants.isChecked()){
+            Toast.makeText(this, "answer question no.6 " + name + "!", Toast.LENGTH_LONG).show();
+        }
+
+        EditText ed4 = findViewById(R.id.editText4);
+        String editText4 = ed4.getText().toString();
+
+        if(!editText4.isEmpty() && editText4 == "Steve jobs"){
+            score = score+1;
+        }
+        else if(editText4.isEmpty()){
+            Toast.makeText(this, "Answer question no.7 " + name + "!", Toast.LENGTH_LONG).show();
+        }
+
+        RadioButton rb7 = findViewById(R.id.radioButton7);
+        RadioButton rb8 = findViewById(R.id.radioButton8);
+        if(rb7.isChecked() && !rb8.isChecked()){
+            score +=1;
+        }else if(!rb7.isChecked() && !rb8.isChecked())
+            Toast.makeText(this, "Answer question no.8 " + name + "!", Toast.LENGTH_LONG).show();
+
 
     }
 }
